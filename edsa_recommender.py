2a1cbae67ecfbd71ad6eaf46742458707298a199
DNVena <60379563+DNVena@users.noreply.github.com>
2020-07-16 12:39:50 +0200

    fix indentation error

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -154,8 +154,9 @@ def main():
             st.title("We think you'll like:")
             for i,j in enumerate(recommended):
                 st.subheader(str(i+1)+'. '+j)
-         if st.button("Register"):
-            st.write('### Enter Your Three Favorite Movies')
+                
+        if st.button("Register"):
+            #st.write('### Enter Your Three Favorite Movies')
             movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
             movie_2 = st.selectbox('Second Option',title_list[25055:25255])
             movie_3 = st.selectbox('Third Option',title_list[21100:21200])
