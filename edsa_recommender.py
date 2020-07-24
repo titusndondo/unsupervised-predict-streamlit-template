9f4ad0a89742688aaa2868c305b1f3f33a9e2f53
DNVena <60379563+DNVena@users.noreply.github.com>
2020-07-24 14:52:29 +0200

    name error

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -177,7 +177,7 @@ def main():
                 return recommended_movies   
             recommended_movie = collab(userId, top_n=10)
             st.title("We think you'll like:")
-            for i,j in enumerate(recommended_movies):
+            for i,j in enumerate(recommended_movie):
                 st.subheader(str(i+1)+'. '+j)
         if st.button("Register"):
             #st.write('### Enter Your Three Favorite Movies')
